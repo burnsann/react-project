b9735ea501e21f8c903cb62a7864615ba8ddd41d
Anna Burnos <burnos.anna@gmail.com>
2024-02-14 17:10:58 +0100

    Add new data to store.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,16 @@
 import styles from './NavBar.module.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.left}>
-        <a href="/"><i className="fa fa-tasks"></i></a>
+        <Link to="/"><i className="fa fa-tasks"></i></Link>
       </div>
       <ul className={styles.right}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/favorite">Favorite</a></li>
-        <li><a href="/about">About</a></li>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></li>
       </ul>
     </nav>
   )
